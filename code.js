53c1442e44cf3faee2d446036b894d2a059a2d16
kaybear307 <knyamaguchi@outlook.com>
2021-08-12 17:10:03 -0500

    Finished with View 2. Next step is to make the input form and submit button functional.

--- code.js
+++ code.js
@@ -1,4 +1,61 @@
-// To run this assignment, right click on index.html in the Visual Studio Code file explorer to the left
-// and select "Open with Live Server"
+//VARIABLES
+const baseURL = 'https://jservice.io/'
+let categoryArray = []
+let randomArray = []
+let startQuestion = []
+let arrayQuestions = []
+let playButton = document.getElementById('playButton')
+let questionDiv = document.getElementById('questionDiv')
+const main = document.getElementById('main')
+let scoreDiv = document.createElement('div')
+scoreDiv.classList.add('scoreDiv')
+let score = ['0']
 
-// YOUR CODE HERE!
+
+
+//FUNCTIONS
+function gameFetch(){
+fetch(`${baseURL}api/random`)
+  .then(response => response.json())
+  .then(categoryData => {
+      categoryArray = categoryData[0].category_id
+
+      fetch(`${baseURL}api/clues?category=${categoryArray}`)
+        .then(response => response.json())
+        .then(data => {
+            randomArray = data
+            questionDiv.append(randomArray[Math.floor(Math.random()*(randomArray.length-1))].question)
+            console.log(data)
+        })
+      console.log(categoryArray)
+      answerInput()
+      scoreCard()
+  });
+};
+
+function answerInput(){
+    const input = document.createElement('input')
+    input.classList.add('answerInput')
+    input.innerText = 'type your answer here.'
+    main.append(input)
+
+    const button = document.createElement('button')
+    button.classList.add('answerButton')
+    button.innerText = 'Feeling Smart!'
+    main.append(button)
+}
+
+function scoreCard(){
+    document.body.append(scoreDiv)
+    scoreDiv.innerHTML = `Score = ${score}`
+}
+
+function clickHandle(event) {
+    event.preventDefault()
+    console.log(input.value)
+}
+
+playButton.addEventListener('click', function(){
+    gameFetch()
+    playButton.style.display = 'none'
+})
