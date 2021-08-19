f36d3d1e375a39366d30838398c80567af73b790
kaybear307 <knyamaguchi@outlook.com>
2021-08-19 09:10:22 -0500

    still having issues with my score card and the loss of my buttons after 2nd click. Pushing for recovery purposes.

--- code.js
+++ code.js
@@ -1,15 +1,24 @@
 //VARIABLES
 const baseURL = 'https://jservice.io/'
-let categoryArray = []
-let randomArray = []
-let startQuestion = []
-let arrayQuestions = []
+let categoryArray = [] //Used in fetch to store a random category
+let randomArray = [] //Used in fetch to store the selected category and stores the arrays up to 100
+let startQuestion = [] // Used in fetch to store the first displayed question array.
+let randomNum = 0
+// let randomNum = Math.floor(Math.random()*(randomArray.length-1))
+
 let playButton = document.getElementById('playButton')
 let questionDiv = document.getElementById('questionDiv')
 const main = document.getElementById('main')
+let input = document.createElement('input')
+let submitButton = document.createElement('button')
+let correctAnsDiv = document.getElementById('correctAnsDiv')
+let nextButton = document.getElementById('nextButton')
+let loserDiv = document.getElementById('loserDiv')
+let playAgain = document.getElementById('playAgainButton')
 let scoreDiv = document.createElement('div')
 scoreDiv.classList.add('scoreDiv')
-let score = ['0']
+let score = 0
+
 
 
 
@@ -24,35 +33,100 @@ fetch(`${baseURL}api/random`)
         .then(response => response.json())
         .then(data => {
             randomArray = data
-            questionDiv.append(randomArray[Math.floor(Math.random()*(randomArray.length-1))].question)
-            console.log(data)
+            startQuestion = randomArray[randomNum]
+            questionDiv.append(startQuestion.question)
+            console.log(randomArray)
+            console.log(startQuestion.answer)
+            
         })
       console.log(categoryArray)
       answerInput()
-      scoreCard()
+    
+      
   });
+  
 };
 
 function answerInput(){
-    const input = document.createElement('input')
     input.classList.add('answerInput')
-    input.innerText = 'type your answer here.'
+    input.placeholder = 'type your answer here.'
     main.append(input)
+    
+    
+    submitButton.classList.add('submitButton')
+    submitButton.innerText = 'Feeling Smart!'
+    main.append(submitButton)
 
-    const button = document.createElement('button')
-    button.classList.add('answerButton')
-    button.innerText = 'Feeling Smart!'
-    main.append(button)
+    submitButton.addEventListener('click', function(event){
+        event.preventDefault()
+        let userAnswer = input.value
+        console.log(userAnswer)
+        formToDisapear()
+        if (userAnswer.toLowerCase() === startQuestion.answer.toLowerCase()) {
+            console.log('true')
+            score += 1
+            scoreCard()
+            return correctAnsDiv.style.display='inline'
+        } else {
+            console.log('false')
+            score = 0
+            scoreCard()
+            return loserDiv.style.display ='inline'
+        }
+        
+    })
+    input.value = ''
+    
 }
 
 function scoreCard(){
     document.body.append(scoreDiv)
     scoreDiv.innerHTML = `Score = ${score}`
+    console.log("score" + score)
 }
 
-function clickHandle(event) {
-    event.preventDefault()
-    console.log(input.value)
+// submitButton.addEventListener('click', function(){
+//     formToDisapear()
+// })
+
+nextButton.addEventListener('click', function(){
+    answerInput()
+    randomNum += 1
+    startQuestion = randomArray[randomNum]
+    questionDiv.innerHTML = ''
+    questionDiv.append(startQuestion.question)
+    console.log(startQuestion.answer)
+    winnerDisapear()
+    formToInline()
+    
+})
+
+playAgain.addEventListener('click', function(){
+    questionDiv.innerHTML = ''
+    gameFetch()
+    loserDisapear()
+    formToInline()
+    console.log(playAgain)
+})
+
+function winnerDisapear(){
+    nextButton.style.display = 'none'
+    correctAnsDiv.style.display = 'none'
+}
+
+function loserDisapear(){
+    loserDiv.style.display = 'none'
+    playAgain.style.display = 'none'
+}
+
+function formToInline(){
+    input.style.display = 'inline'
+    submitButton.style.display = 'inline'
+}
+
+function formToDisapear(){
+    input.style.display = 'none'
+    submitButton.style.display = 'none'
 }
 
 playButton.addEventListener('click', function(){
